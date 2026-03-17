233826ece04fb8c0d90a63c6be8be577d23a7141
Bhuvana-31 <bhuvanabcagasc@gmail.com>
2026-03-17 15:56:09 +0530

    added appspec file

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# pull the docker image from docker hub
+docker pull bhuvana31/simple-python-flask-project:latest
+
+# Run the docker image as a container
+docker run -d -p 5000:5000 bhuvana31/simple-python-flask-project:latest
